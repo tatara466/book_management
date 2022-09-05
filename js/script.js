1463ed82f055950d4ad90f8baa28b524d83951c5
tatara466 <31890490+tatara466@users.noreply.github.com>
2022-09-05 11:23:20 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -8,4 +8,205 @@ $(function() {
 	$(".book3").hover(function() {
 		$(".Product_image").attr("src","https://images-fe.ssl-images-amazon.com/images/P/4297130122.09.LZZZZZZZ");
 	});
+    $(".book4").hover(function() {
+		$(".Product_image").attr("src","https://images-fe.ssl-images-amazon.com/images/P/4295008435.09.LZZZZZZZ");
+	});
+    $(".book5").hover(function() {
+		$(".Product_image").attr("src","https://images-fe.ssl-images-amazon.com/images/P/4478108242.09.LZZZZZZZ");
+	});
+    $(".book6").hover(function() {
+		$(".Product_image").attr("src","https://images-fe.ssl-images-amazon.com/images/P/4046018631.09.LZZZZZZZ");
+	});
+    $(".book7").hover(function() {
+		$(".Product_image").attr("src","https://images-fe.ssl-images-amazon.com/images/P/4815602840.09.LZZZZZZZ");
+	});
+    $(".book8").hover(function() {
+		$(".Product_image").attr("src","https://images-fe.ssl-images-amazon.com/images/P/4478105251.09.LZZZZZZZ");
+	});
+    $(".book9").hover(function() {
+		$(".Product_image").attr("src","https://images-fe.ssl-images-amazon.com/images/P/4295011231.09.LZZZZZZZ");
+	});
+    $(".book10").hover(function() {
+		$(".Product_image").attr("src","https://images-fe.ssl-images-amazon.com/images/P/4295005231.09.LZZZZZZZ");
+	});
+    $(".book11").hover(function() {
+		$(".Product_image").attr("src","https://images-fe.ssl-images-amazon.com/images/P/4295003093.09.LZZZZZZZ");
+	});
+    $(".book12").hover(function() {
+		$(".Product_image").attr("src","https://images-fe.ssl-images-amazon.com/images/P/4295007285.09.LZZZZZZZ");
+	});
+    $(".book13").hover(function() {
+		$(".Product_image").attr("src","https://images-fe.ssl-images-amazon.com/images/P/4295012491.09.LZZZZZZZ");
+	});
+    $(".book14").hover(function() {
+		$(".Product_image").attr("src","https://images-fe.ssl-images-amazon.com/images/P/4910088121.09.LZZZZZZZ");
+	});
+    $(".book15").hover(function() {
+		$(".Product_image").attr("src","https://images-fe.ssl-images-amazon.com/images/P/4478109206.09.LZZZZZZZ");
+	});
+    $(".book16").hover(function() {
+		$(".Product_image").attr("src","https://images-fe.ssl-images-amazon.com/images/P/4799320599.09.LZZZZZZZ");
+	});
+    $(".book17").hover(function() {
+		$(".Product_image").attr("src","https://images-fe.ssl-images-amazon.com/images/P/4757414676.09.LZZZZZZZ");
+	});
+    $(".book18").hover(function() {
+		$(".Product_image").attr("src","https://images-fe.ssl-images-amazon.com/images/P/4757413319.09.LZZZZZZZ");
+	});
+    $(".book19").hover(function() {
+		$(".Product_image").attr("src","https://images-fe.ssl-images-amazon.com/images/P/4757413688.09.LZZZZZZZ");
+	});
+    $(".book20").hover(function() {
+		$(".Product_image").attr("src","https://images-fe.ssl-images-amazon.com/images/P/4761275774.09.LZZZZZZZ");
+	});
+    $(".book21").hover(function() {
+		$(".Product_image").attr("src","https://images-fe.ssl-images-amazon.com/images/P/4815610215.09.LZZZZZZZ");
+	});
+    $(".book22").hover(function() {
+		$(".Product_image").attr("src","https://images-fe.ssl-images-amazon.com/images/P/4910017224.09.LZZZZZZZ");
+	});
+    $(".book23").hover(function() {
+		$(".Product_image").attr("src","https://images-fe.ssl-images-amazon.com/images/P/4478112673.09.LZZZZZZZ");
+	});
+    $(".book24").hover(function() {
+		$(".Product_image").attr("src","https://images-fe.ssl-images-amazon.com/images/P/4833424533.09.LZZZZZZZ");
+	});
+    $(".book25").hover(function() {
+		$(".Product_image").attr("src","https://images-fe.ssl-images-amazon.com/images/P/4198654387.09.LZZZZZZZ");
+	});
+    $(".book26").hover(function() {
+		$(".Product_image").attr("src","https://images-fe.ssl-images-amazon.com/images/P/4479796320.09.LZZZZZZZ");
+	});
+    $(".book27").hover(function() {
+		$(".Product_image").attr("src","https://images-fe.ssl-images-amazon.com/images/P/4801481051.09.LZZZZZZZ");
+	});
+    $(".book28").hover(function() {
+		$(".Product_image").attr("src","https://images-fe.ssl-images-amazon.com/images/P/4827213240.09.LZZZZZZZ");
+	});
+    $(".book29").hover(function() {
+		$(".Product_image").attr("src","https://images-fe.ssl-images-amazon.com/images/P/4827212619.09.LZZZZZZZ");
+	});
+    $(".book30").hover(function() {
+		$(".Product_image").attr("src","https://images-fe.ssl-images-amazon.com/images/P/4827211698.09.LZZZZZZZ");
+	});
+    $(".book31").hover(function() {
+		$(".Product_image").attr("src","https://images-fe.ssl-images-amazon.com/images/P/4910216073.09.LZZZZZZZ");
+	});
+    $(".book32").hover(function() {
+		$(".Product_image").attr("src","https://images-fe.ssl-images-amazon.com/images/P/4569852734.09.LZZZZZZZ");
+	});
+    $(".book33").hover(function() {
+		$(".Product_image").attr("src","https://images-fe.ssl-images-amazon.com/images/P/4827211965.09.LZZZZZZZ");
+	});
+    $(".book34").hover(function() {
+		$(".Product_image").attr("src","https://images-fe.ssl-images-amazon.com/images/P/4262174794.09.LZZZZZZZ");
+	});
+    $(".book35").hover(function() {
+		$(".Product_image").attr("src","https://images-fe.ssl-images-amazon.com/images/P/4046045698.09.LZZZZZZZ");
+	});
+    $(".book36").hover(function() {
+		$(".Product_image").attr("src","https://images-fe.ssl-images-amazon.com/images/P/4827212147.09.LZZZZZZZ");
+	});
+    $(".book37").hover(function() {
+		$(".Product_image").attr("src","https://images-fe.ssl-images-amazon.com/images/P/4802613776.09.LZZZZZZZ");
+	});
+    $(".book38").hover(function() {
+		$(".Product_image").attr("src","https://images-fe.ssl-images-amazon.com/images/P/4478372608.09.LZZZZZZZ");
+	});
+    $(".book39").hover(function() {
+		$(".Product_image").attr("src","https://images-fe.ssl-images-amazon.com/images/P/4802613199.09.LZZZZZZZ");
+	});
+    $(".book40").hover(function() {
+		$(".Product_image").attr("src","https://images-fe.ssl-images-amazon.com/images/P/4296107631.09.LZZZZZZZ");
+	});
+    $(".book41").hover(function() {
+		$(".Product_image").attr("src","https://images-fe.ssl-images-amazon.com/images/P/4065165369.09.LZZZZZZZ");
+	});
+    $(".book42").hover(function() {
+		$(".Product_image").attr("src","https://images-fe.ssl-images-amazon.com/images/P/4502168114.09.LZZZZZZZ");
+	});
+    $(".book43").hover(function() {
+		$(".Product_image").attr("src","https://images-fe.ssl-images-amazon.com/images/P/4774163015.09.LZZZZZZZ");
+	});
+    $(".book44").hover(function() {
+		$(".Product_image").attr("src","https://images-fe.ssl-images-amazon.com/images/P/4799106945.09.LZZZZZZZ");
+	});
+    $(".book45").hover(function() {
+		$(".Product_image").attr("src","https://images-fe.ssl-images-amazon.com/images/P/4502342416.09.LZZZZZZZ");
+	});
+    $(".book46").hover(function() {
+		$(".Product_image").attr("src","https://images-fe.ssl-images-amazon.com/images/P/4799328506.09.LZZZZZZZ");
+	});
+    $(".book47").hover(function() {
+		$(".Product_image").attr("src","https://images-fe.ssl-images-amazon.com/images/P/4837804985.09.LZZZZZZZ");
+	});
+    $(".book48").hover(function() {
+		$(".Product_image").attr("src","https://images-fe.ssl-images-amazon.com/images/P/4478109575.09.LZZZZZZZ");
+	});
+    $(".book49").hover(function() {
+		$(".Product_image").attr("src","https://images-fe.ssl-images-amazon.com/images/P/4495540890.09.LZZZZZZZ");
+	});
+    $(".book50").hover(function() {
+		$(".Product_image").attr("src","https://images-fe.ssl-images-amazon.com/images/P/4866365080.09.LZZZZZZZ");
+	});
+    $(".book51").hover(function() {
+		$(".Product_image").attr("src","https://images-fe.ssl-images-amazon.com/images/P/4799313134.09.LZZZZZZZ");
+	});
+    $(".book52").hover(function() {
+		$(".Product_image").attr("src","https://images-fe.ssl-images-amazon.com/images/P/4761273070.09.LZZZZZZZ");
+	});
+    $(".book53").hover(function() {
+		$(".Product_image").attr("src","https://images-fe.ssl-images-amazon.com/images/P/4492534032.09.LZZZZZZZ");
+	});
+    $(".book54").hover(function() {
+		$(".Product_image").attr("src","https://images-fe.ssl-images-amazon.com/images/P/4478026122.09.LZZZZZZZ");
+	});
+    $(".book55").hover(function() {
+		$(".Product_image").attr("src","https://images-fe.ssl-images-amazon.com/images/P/4799323628.09.LZZZZZZZ");
+	});
+    $(".book56").hover(function() {
+		$(".Product_image").attr("src","https://images-fe.ssl-images-amazon.com/images/P/4344039777.09.LZZZZZZZ");
+	});
+    $(".book57").hover(function() {
+		$(".Product_image").attr("src","https://images-fe.ssl-images-amazon.com/images/P/4478069395.09.LZZZZZZZ");
+	});
+    $(".book58").hover(function() {
+		$(".Product_image").attr("src","https://images-fe.ssl-images-amazon.com/images/P/4833424118.09.LZZZZZZZ");
+	});
+    $(".book59").hover(function() {
+		$(".Product_image").attr("src","https://images-fe.ssl-images-amazon.com/images/P/4413019164.09.LZZZZZZZ");
+	});
+    $(".book60").hover(function() {
+		$(".Product_image").attr("src","https://images-fe.ssl-images-amazon.com/images/P/4766730275.09.LZZZZZZZ");
+	});
+    $(".book61").hover(function() {
+		$(".Product_image").attr("src","https://images-fe.ssl-images-amazon.com/images/P/4860634187.09.LZZZZZZZ");
+	});
+    $(".book62").hover(function() {
+		$(".Product_image").attr("src","https://images-fe.ssl-images-amazon.com/images/P/4799320238.09.LZZZZZZZ");
+	});
+    $(".book63").hover(function() {
+		$(".Product_image").attr("src","https://images-fe.ssl-images-amazon.com/images/P/4905073618.09.LZZZZZZZ");
+	});
+    $(".book64").hover(function() {
+		$(".Product_image").attr("src","https://images-fe.ssl-images-amazon.com/images/P/4909417958.09.LZZZZZZZ");
+	});
+    $(".book65").hover(function() {
+		$(".Product_image").attr("src","https://images-fe.ssl-images-amazon.com/images/P/4769610041.09.LZZZZZZZ");
+	});
+    $(".book66").hover(function() {
+		$(".Product_image").attr("src","https://images-fe.ssl-images-amazon.com/images/P/4569832059.09.LZZZZZZZ");
+	});
+    $(".book67").hover(function() {
+		$(".Product_image").attr("src","https://images-fe.ssl-images-amazon.com/images/P/4903722848.09.LZZZZZZZ");
+	});
+    $(".book68").hover(function() {
+		$(".Product_image").attr("src","https://images-fe.ssl-images-amazon.com/images/P/4799310380.09.LZZZZZZZ");
+	});
+    $(".book69").hover(function() {
+		$(".Product_image").attr("src","https://images-fe.ssl-images-amazon.com/images/P/4478084947.09.LZZZZZZZ");
+	});
+    $(".book70").hover(function() {
+		$(".Product_image").attr("src","https://images-fe.ssl-images-amazon.com/images/P/4798175307.09.LZZZZZZZ");
+	});
 });
