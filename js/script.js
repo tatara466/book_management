cb0a2b36c8de8a882668abeccb0a3d7f071382d6
tatara466 <31890490+tatara466@users.noreply.github.com>
2022-09-05 10:11:23 +0900

    tatara

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 $(function() {
 	$(".book1").hover(function() {
-		$(".change_image").attr("src","images/画像2.png");
+		$(".Product_image").attr("src","https://images-fe.ssl-images-amazon.com/images/P/4297103087.09.LZZZZZZZ");
 	});
 	$(".book2").hover(function() {
-		$(".change_image").attr("src","images/画像3.png");
+		$(".Product_image").attr("src","https://images-fe.ssl-images-amazon.com/images/P/4866671734.09.LZZZZZZZ");
 	});
 	$(".book3").hover(function() {
-		$(".change_image").attr("src","images/画像4.png");
+		$(".Product_image").attr("src","https://images-fe.ssl-images-amazon.com/images/P/4297130122.09.LZZZZZZZ");
 	});
 });
